87d49dd1bfae2dc94e1b3c231e6b5c979e94c4cd
Martin Magana <magana.web@gmail.com>
2015-04-08 20:43:59 -0700

    added highlighting
    add simple highlighting for text. it's not exactly how I want it,
    but a good enough bookmark for a commit.

--- popup_html.js
+++ popup_html.js
@@ -3,5 +3,9 @@
 // http://www.tinymce.com/wiki.php/Configuration
 // http://www.tinymce.com/wiki.php/Controls
 tinymce.init({
-    selector: "#editor"
+    selector: "#editor",
+    style_formats_merge:true,
+    style_formats: [
+        {title: 'Highlight', block: 'span', styles: {'background-color': 'yellow'}},
+    ]
 });
