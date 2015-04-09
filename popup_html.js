f5835260e6931c2c257229e57b9b85aec69e57ea
Martin Magana <magana.web@gmail.com>
2015-04-08 19:51:19 -0700

    added tinymce editor
    popup.html has the tinymce installeds

---
+++ popup_html.js
@@ -0,0 +1,7 @@
+// TinyMCE configuration
+// http://www.tinymce.com/wiki.php/Advanced
+// http://www.tinymce.com/wiki.php/Configuration
+// http://www.tinymce.com/wiki.php/Controls
+tinymce.init({
+    selector: "#editor"
+});
